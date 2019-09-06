35bc88fa2327d07c137c58d26a8b8315ea993ef3
Olga Smirnova <os_smirnova92@mail.ru>
2019-09-06 15:03:56 +0300

    правки, связанные с добавлением блока .nav

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-/* alert("Hello world!") */
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    var nav = document.querySelector('.nav');
+
+    function showScrollMenu (menu) {
+        var screenHeight = window.screen.height;
+
+        window.addEventListener('scroll', function() {
+            if(window.pageYOffset > screenHeight - 100) {
+                menu.classList.add('active');
+            } else {
+                menu.classList.remove('active');
+            }
+        });
+    };
+
+    showScrollMenu(nav);
+
+});
+	
